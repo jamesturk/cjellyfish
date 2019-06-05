d05f16d53a90408d4b2bc0450ed5029096eca71c
James Turk <dev@jamesturk.net>
2019-06-05 10:49:37 -0400

    fix trailing W in metaphone impl, ANDREW->ANTR

--- metaphone.c
+++ metaphone.c
@@ -161,7 +161,7 @@ char* metaphone(const char *str)
             if (s == str && next == 'h') {
                 next = tolower(*(++s + 1));
             }
-            if (ISVOWEL(next) || next == 0) {
+            if (ISVOWEL(next)) {
                 *r++ = 'W';
             }
             break;
